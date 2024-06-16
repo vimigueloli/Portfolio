e975615f6a33321ffb153ed6d002c1b6e69fd569
victor <victor.miguel@devio.com.br>
2024-06-16 02:10:13 -0300

    new - api integrations

--- src/components/project/index.tsx
+++ src/components/project/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 interface RepoProps {
     id: any;
@@ -29,9 +30,50 @@ interface ProjectProps {
 }
 
 export default function Project({ id, project }: ProjectProps) {
+    function handleTitles(title: string) {
+        let input = title.replace("_", " ");
+        input = input.replace("-", " ");
+        let titles = input.split(" ");
+        let output = "";
+        titles.forEach((item: string) => {
+            output += item[0].toUpperCase() + item.substring(1);
+        });
+        output = output.replace(/([a-z])([A-Z])/g, "$1 $2");
+        output = output.replace(/([A-Z])([A-Z][a-z])/g, "$1 $2");
+        return output.trim();
+    }
+
     return (
-        <div className="line-center w-72 h-96 rounded-md border border-white text-white">
-            {project.name}
-        </div>
+        <a
+            href={project.homepageUrl}
+            target="_blank"
+            title={`Abrir ${handleTitles(project.name)}`}
+            className="line-between overflow-hidden relative flex-col w-96 h-96 rounded-xl border border-white text-white cursor-pointer hover:scale-110 with-transition"
+        >
+            <div className="w-full h-full line-right items-end absolute p-4 ">
+                <a
+                    href={project.url}
+                    target="_blank"
+                    title={`Repositório ${handleTitles(project.name)}`}
+                    className="w-10 h-10 rounded-md bg-black/50 backdrop-blur-xl border border-transparent with-transition shadow-xl z-50 line-center hover:border-white"
+                >
+                    <FaGithub size={25} />
+                </a>
+            </div>
+            <div className="line-center w-full pt-4 font-semibold text-2xl">
+                {handleTitles(project.name)}
+            </div>
+            <div className="line-center text-justify px-4 text-xs">
+                {project.shortDescriptionHTML}
+            </div>
+            <a
+                href={project.homepageUrl}
+                target="_blank"
+                title={`Abrir ${handleTitles(project.name)}`}
+                className="w-full h-2/3 items-end line-center"
+            >
+                <img src={project.openGraphImageUrl} />
+            </a>
+        </a>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,34 @@ import api from "@/services/api";
 import Projects from "@/components/projects";
 
 export default function Home() {
+    const [data, setData] = useState<any>();
+    useEffect(() => {
+        async function loadUserData() {
+            try {
+                const response = await api.get(`users/${process.env.USER}`);
+                console.log("user->", response.data);
+                setData(response.data);
+            } catch (e: any) {
+                console.log("failed to load user data ->", e);
+            }
+        }
+        loadUserData();
+    }, []);
+
     return (
         <main className={`bg-black w-screen h-screen overflow-auto`}>
-            <div className="text-white text-3xl">User Data</div>
-            <div className="line-center gap-16 flex-wrap">
+            <div className="text-white line-center flex-wrap gap-8 text-3xl my-12">
+                <div className="line-center w-full">
+                    <div className="w-32 h-32 rounded-full border overflow-hidden border-white">
+                        <img src={data.avatar_url} />
+                    </div>
+                </div>
+                <div className="w-full line-center">{data.name}</div>
+                <div className="w-full line-center px-12 text-justify text-xl">
+                    {data.bio}
+                </div>
+            </div>
+            <div className="line-center gap-16 flex-wrap pb-32">
                 <Projects />
             </div>
         </main>
